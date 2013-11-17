e8dc08fb162bb1b3c1c2f85760554e9df601fc10
Dmitriy Zadoroghniy <mobdim@gmail.com>
2013-11-17 16:56:33 +0400

    Add iPhone5 determine.

--- Helpers/Common.h
+++ Helpers/Common.h
@@ -14,4 +14,8 @@
 #define CHECK(val)              NSAssert((val), @#val)
 #define CHECKCLASS(val, cl)     CHECK([val isKindOfClass:[cl class]])
 
+#define isPhone568                  ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone && [UIScreen mainScreen].bounds.size.height == 568)
+#define iPhone568ImageNamed(image)  (isPhone568 ? [NSString stringWithFormat:@"%@-568h.%@", [image stringByDeletingPathExtension], [image pathExtension]] : image)
+#define iPhone568Image(image)       ([UIImage imageNamed:iPhone568ImageNamed(image)])
+
 #endif
