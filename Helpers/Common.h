a7d151b2b4c239fd94fc541230e2c00c55171aa0
Dmitriy Zadoroghniy <mobdim@gmail.com>
2013-11-27 17:50:21 +0400

    Add defines for block test.

--- Helpers/Common.h
+++ Helpers/Common.h
@@ -18,6 +18,28 @@
 #define iPhone568ImageNamed(image)  (isPhone568 ? [NSString stringWithFormat:@"%@-568h.%@", [image stringByDeletingPathExtension], [image pathExtension]] : image)
 #define iPhone568Image(image)       ([UIImage imageNamed:iPhone568ImageNamed(image)])
 
+// ******* Tests +++
+
+// Macro - Set the flag for block completion
+#define StartBlock() __block BOOL waitingForBlock = YES
+
+// Macro - Set the flag to stop the loop
+#define EndBlock() waitingForBlock = NO
+
+// Macro - Wait and loop until flag is set
+#define WaitUntilBlockCompletes() WaitWhile(waitingForBlock)
+
+// Macro - Wait for condition to be NO/false in blocks and asynchronous calls
+// Each test should have its own instance of a BOOL condition because of non-thread safe operations
+#define WaitWhile(condition) \
+do { \
+while(condition) { \
+[[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]]; \
+} \
+} while(0)
+
+// ******* Tests ---
+
 #import "DDLog.h"
 #import "DDASLLogger.h"
 #import "DDTTYLogger.h"
