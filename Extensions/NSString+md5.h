e409c8de09037b5bd2bb910bc7d149dd4b7e2bb1
Dmitriy Zadoroghniy <mobdim@gmail.com>
2014-01-08 22:51:43 +0400

    Add NSString+md5.

---
+++ Extensions/NSString+md5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+md5.h
+//  ipPort.New
+//
+//  Created by Dmitriy Zadoroghnyy on 08.01.14.
+//  Copyright (c) 2014 ipPort. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (md5)
+
++ (NSString *) md5:(NSString *)str;
+
+@end
